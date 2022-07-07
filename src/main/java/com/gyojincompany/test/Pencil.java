cee2c9a4e840e4e2dd6550df642e07621c629c46
ATE <ATE@DESKTOP-ODIPDNU>
2022-07-07 11:57:28 +0900

    스프링 DI 활용 예제

---
+++ src/main/java/com/gyojincompany/test/Pencil.java
@@ -0,0 +1,5 @@
+package com.gyojincompany.test;
+
+public interface Pencil {
+	void use();
+}
---
+++ src/main/java/com/gyojincompany/test/Pencil6BwithEraser.java
@@ -0,0 +1,11 @@
+package com.gyojincompany.test;
+
+public class Pencil6BwithEraser implements Pencil {
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		System.out.println("6B 굵기로 쓰이고 지우개도 있습니다!");
+	}	
+
+}
